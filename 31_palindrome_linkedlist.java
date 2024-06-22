b6e40176ba715b7c0869c816500f8ba8ca76a275
Jalaj Pandey <jalajpandey123@gmail.com>
2024-06-22 10:42:36 +0530

    32 ques

--- 31_palindrome_linkedlist.java
+++ 31_palindrome_linkedlist.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
         Stack<Integer> stack = new Stack();
---
+++ 32_palindrome_substring.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int ans = 0;
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(isPalindrome(s,i,j)) ans++;
+            }
+        }
+        return ans;
+    }
+    public boolean isPalindrome(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left++) != s.charAt(right--)) return false;
+        }
+        return true;
+    }
+}
